b2580207be332d51f0bf890203866ff91f8f11b2
roddyka <rodrigoka@live.com>
2017-08-08 21:52:04 -0300

    Projetos da aula 05 - REGEX, OO, validação

--- Aula05/Projeto03/Program.cs
+++ Aula05/Projeto03/Program.cs
@@ -10,6 +10,39 @@ namespace Projeto03
     {
         static void Main(string[] args)
         {
+            Queue<string> fila = new Queue<string>();
+
+            fila.Enqueue("Joao");
+            fila.Enqueue("Ana");
+            fila.Enqueue("Jose");
+            fila.Enqueue("Maria");
+
+            //sem remover
+            Console.WriteLine("primeiro da fila: " + fila.Peek());
+
+            //removendo
+            Console.WriteLine("primeiro da fila: " + fila.Dequeue());
+
+            foreach (string item in fila)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n PILHA \n");
+            //pilha
+            Stack<string> pilha = new Stack<string>();
+            pilha.Push("Antonio");
+            pilha.Push("Seila");
+            pilha.Push("Teste");
+            pilha.Push("Zueira");
+
+            foreach(string item in pilha)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("primeiro da pilha: " + pilha.Peek());
+
             Console.ReadKey();
         }
     }
